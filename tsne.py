2f8aeede0dc7f6df8b2eb7a67d86a7328b6c6ed7
Andrew Draganov <adraganov@exptechinc.com>
2020-09-13 21:08:57 -0400

    tsne runs but doesn't account for sigmas or perplexity yet, so it learns to space everything out evenly

--- tsne.py
+++ tsne.py
@@ -1,9 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mnist import MNIST
 
-def pairwise_euclidean(points):
+def get_deltas(points):
+    points = points.astype(np.float32)
     points_zero = np.expand_dims(points, axis=0)
     points_one = np.expand_dims(points, axis=1)
-    return np.sum((points_zero - points_one)**2, axis=-1)
+    return (points_zero - points_one)
+
+def normalize_points(points):
+    points = points / np.amax(points)
+    return points
+
+def pairwise_euclidean(points, keepdims=False):
+    deltas = get_deltas(points)
+    dists = np.sum(deltas**2, axis=-1, keepdims=keepdims)
+    assert dists[0, 0] == 0
+    return dists
 
 def remove_diag(x):
     x_no_diag = np.ndarray.flatten(x)
@@ -12,13 +25,15 @@ def remove_diag(x):
     return x_no_diag
 
 def high_dim_probs(points):
+    points = normalize_points(points)
     probabilities = np.zeros((len(points), len(points)))
     pairwise_dists = pairwise_euclidean(points)
     # FIXME binary search for sigma
     sigma_i = np.ones((len(points), 1))
     numerator = np.exp(-1 * pairwise_dists / (2 * sigma_i)) 
-    # FIXME should I remove one from the sums to account for the distance from a point to itself?
-    denominator = np.sum(np.exp(-1 * pairwise_dists / (2 * sigma_i)), axis=1, keepdims=True) - 1
+    # Subtract one so that we're not comparing point to itself in denominator sum
+    denominator = np.sum(np.exp(-1 * pairwise_dists / (2 * sigma_i))) - len(points)
+    assert denominator != 0
     p_j_given_i = numerator / denominator
     # Zero out probability from point to itself
     p_j_given_i -= np.eye(len(p_j_given_i)) * p_j_given_i
@@ -28,20 +43,40 @@ def high_dim_probs(points):
 def low_dim_probs(points):
     probabilities = np.zeros((len(points), len(points)))
     pairwise_dists = pairwise_euclidean(points)
-    # FIXME binary search for sigma
-    sigma_i = np.ones((len(points), 1))
-    numerator = np.pow(1 + pairwise_dists / (2 * sigma_i), -1) 
-    # FIXME should I remove one from the sums to account for the distance from a point to itself?
-    denominator = np.sum(np.exp(-1 * pairwise_dists / (2 * sigma_i)), axis=1, keepdims=True) - 1
-    q_j_given_i = numerator / denominator
-    # Zero out probability from point to itself
-    q_j_given_i -= np.eye(len(q_j_given_i)) * q_j_given_i
-    Q = (q_j_given_i + np.transpose(q_j_given_i)) / 2
-    return P
+    numerator = np.power(1 + pairwise_dists, -1) 
+    # Remove similarity from point to itself
+    numerator -= np.eye(len(numerator))
+    denominator = np.sum(np.power(1 + pairwise_dists, -1), axis=1, keepdims=True) - 1
+    Q = numerator / denominator
+    return Q
 
 
 if __name__ == '__main__':
-    points = np.random.rand(2000, 16)
-    P = high_dim_probs(points)
+    mndata = MNIST('./python-mnist/data')
+    images, labels = mndata.load_training()
+    images = np.array(images)
+    labels = np.array(labels)
+    images = images[::100, :]
+    labels = labels[::100]
+    y = np.random.rand(len(images), 2)
+    P = high_dim_probs(images)
+    lr = 100
+    momentum = 0.5
+    old_y = y
 
+    # Gradient descent loop
+    for i in range(1000):
+        Q = low_dim_probs(y)
+        prob_diff = np.expand_dims(P - Q, axis=-1)
+        deltas = get_deltas(y)
+        Z = np.power(1 + pairwise_euclidean(y, keepdims=True), -1)
+        all_grads = prob_diff * deltas * Z
+        gradient = 4 * np.sum(all_grads, axis=1)
 
+        temp = y
+        y_delta = y - old_y
+        y = y + lr * gradient + momentum * y_delta
+        old_y = temp
+
+    plt.scatter(y[:, 0], y[:, 1], c=labels)
+    plt.show()
